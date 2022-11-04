1c95edca4e9533e738d2ba415158d9e83b6a9e48
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-04 15:38:26 -0400

    express added and example of middleware funcitons

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const http = require('http');   // importing core modules with names
-const routes = require('./routes'); // importing custom module by relative path
+const express = require('express');
 
-console.log(routes.someText);   // accessing property of routes which are exported
+// express application => middlewares
+const app = express();
+
+// use() allow to add new middleware function which runs on every incoming requests on server.
+// next() allows the request to move on next middleware in line
+app.use((req, res, next) => {
+    console.log("in the middleware");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("in the another middleware");
+});
+
 
 // creating server
-const server = http.createServer(routes.handler);   // creating server and handling requests with routes.handler property
+// creating server and handling requests with routes.handler property
+const server = http.createServer(app);
 
 // server is listening on port 3000
 server.listen(3000);
