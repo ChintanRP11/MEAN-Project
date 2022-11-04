689bcd6569610d13f9b5e1e3d1ec15500adeaf29
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-04 16:00:07 -0400

    server creation code changed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const http = require('http');   // importing core modules with names
 const express = require('express');
 
 // express application => middlewares
 const app = express();
 
-// use() allow to add new middleware function which runs on every incoming requests on server.
+// use() allows to add new middleware function which runs on every incoming requests on server.
 // next() allows the request to move on next middleware in line
 app.use((req, res, next) => {
     console.log("in the middleware");
@@ -12,12 +11,16 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
     console.log("in the another middleware");
+    res.send("<h1>Hello from express middleware!</h1>") // allows to send response with html text and adds required html code.
 });
 
 
-// creating server
-// creating server and handling requests with routes.handler property
-const server = http.createServer(app);
+// // creating server
+// // creating server and handling requests with routes.handler property
+// const server = http.createServer(app);
 
-// server is listening on port 3000
-server.listen(3000);
+// // server is listening on port 3000
+// server.listen(3000);
+
+//express js shorten the server creation code
+app.listen(3000);
