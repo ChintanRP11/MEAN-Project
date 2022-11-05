6e583a91cfb3c6c6310c681e63b38fd6122c05ed
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-05 19:08:39 -0400

    HTML converted to EJS
    EJS used for templating.
    views folder contains ejs files for rendering.
    views/includes contains some same code of ejs which can be resued in other files are seperated.

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,24 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use(express.static(path.join(rootDir,'public')));
+// router.use(express.static(path.join(rootDir,'public')));
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+      });
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -5,11 +5,18 @@ const rootDir = require('../util/path');
 const router = express.Router();
 const adminData = require('./admin');
 
-router.use(express.static(path.join(rootDir,'public')));
+// router.use(express.static(path.join(rootDir,'public')));
 
 router.get('/',(req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+  });
 });
 
 module.exports = router;
