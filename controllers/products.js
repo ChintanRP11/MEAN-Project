b32cf327661af7367ac9bd51c9c3864ed0cdf1cf
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-06 01:12:27 -0400

    MVC Model added
    -models contais model of different class which can be use for data organization.

--- controllers/products.js
+++ controllers/products.js
@@ -18,7 +18,7 @@ exports.postAddProduct = (req,res,next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll( (products)=>{
+    const products = Product.fetchAll( products => {
         res.render('shop', {
             prods: products,
             pageTitle: 'Shop',
--- models/product.js
+++ models/product.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename),'data','products.json'); 
+
 const products = [];
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 
 module.exports = class Product {
     constructor(t) {
@@ -9,14 +21,7 @@ module.exports = class Product {
     }
 
     save() {
-        // products.push(this);
-        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json'); 
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            console.log(fileContent);
+         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -25,12 +30,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json'); 
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
