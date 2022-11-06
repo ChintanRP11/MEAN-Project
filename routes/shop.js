9f8b8b131c966758a200cfbf1db2caf7114e59d7
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-05 23:50:28 -0400

    MVC - controller applied
    MVC structure used in this project
    Controller introduced to application - handles all logic between models and views
    
    Views are already introduced

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,9 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const productsController = require('../controllers/products');
 
-const rootDir = require('../util/path');
 const router = express.Router();
-const adminData = require('./admin');
 
-// router.use(express.static(path.join(rootDir,'public')));
-
-router.get('/',(req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
