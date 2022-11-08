0a21986c86de31f120d6f0fc6603d4905a0586ee
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-08 13:12:19 -0500

    MySQL queries added
    -fetch all products
    -fetch specific product
    -add product
    queries added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,15 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll( products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
-      });
-    });
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('admin/products', {
+                prods: rows,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getAddProduct = (req, res, next) => {
@@ -24,8 +26,11 @@ exports.postAddProduct = (req,res,next) => {
     const price = req.body.price;
     const description = req.body.description;
     const product = new Product(null, title, imageUrl, price, description);
-    product.save();
-    res.redirect('/'); 
+    product.save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
