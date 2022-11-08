0e7bf856e883234eb43991c01d509768ba2b4635
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-08 14:24:13 -0500

    Added: fetching products from database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,14 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
+    Product.findAll()
+        .then(products => {
             res.render('admin/products', {
-                prods: rows,
+                prods: products,
                 pageTitle: 'Admin Products',
                 path: '/admin/products'
             });
-        })
-        .catch(err => console.log(err));
+        }).catch(err => console.log(err));
 }
 
 exports.getAddProduct = (req, res, next) => {
@@ -25,12 +24,14 @@ exports.postAddProduct = (req,res,next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(null, title, imageUrl, price, description);
-    product.save()
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch(err => console.log(err));
+    Product.create({
+        title: title,
+        price: price,
+        imageUrl: imageUrl,
+        description: description
+    }).then(result => {
+        console.log('Created Product');
+    }).catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
