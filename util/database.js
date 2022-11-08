9d1b48ee25a9237cb9ef649eba0b593687acdd84
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-08 13:53:58 -0500

    sequelize added
    -npm sequelize package added
    -configure sequelize in databse.js file
    -creation of model using sequelize
    -synchronizing the databse with models of sequelize before starting the server

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'MySQL@123'
+
+const sequelize = new Sequelize('node-sequelize', 'root', 'MySQL@123', {
+    dialect: 'mysql', 
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
