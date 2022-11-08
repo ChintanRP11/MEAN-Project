fb3b3a26f6a064fc3bae19eec6e9cb1fbeeec59b
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-08 16:52:36 -0500

    Added: User model added
    -User and Product associated
    -User can edit delete add product

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,22 +18,42 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));;
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// associations
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 // sync all models to SQL database: create tables according to models if not exists
 sequelize
     .sync()
     .then(result => {
+        return User.findByPk(1);
+        
+    }).then(user => {
+        if (!user) {
+            return User.create({name: 'Chintan', email: 'test@test.com'});
+        }
+        return user;
+    }).then(user => {
+        // console.log(user);
         app.listen(3000);
-    })
-    .catch(err => console.log(err));
+    }).catch(err => console.log(err));
 
 
 
