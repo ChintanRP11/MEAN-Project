652002526939311ec4727677d5b90ba2312a8c8e
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-11-14 14:08:47 -0500

    Setup for Mongoose
    Commenting, cleaningo and deleting some unrelated files for mongoose.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,12 +32,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    // if (req.user){
+mongoose.connect('mongodb+srv://chintan-mongodb:7I5VwbOKb5jMMVVi@project-mean-stack.6ifabul.mongodb.net/?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(3000);
+    }).catch(err => console.log(err));
 
-    // }
-    app.listen(3000);
-});
 
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,19 +7,19 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+// router.get('/products', shopController.getProducts);
 
-// dynamic routing
-router.get('/products/:productId', shopController.getProduct);
+// // dynamic routing
+// router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+// router.get('/orders', shopController.getOrders);
 
-router.post('/create-order', shopController.postOrder);
+// router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
