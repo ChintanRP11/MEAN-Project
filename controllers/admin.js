9655b8f901d7b52a1c330ca3219ce0a9c82881a4
Chintan Pansuriya <57788556+ChintanRP11@users.noreply.github.com>
2022-12-18 15:58:09 -0500

    product data change authorization added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({userId: req.user._id})
         // .select('title price -_id')
         .populate('userId', 'name')
         .then(products => {
@@ -52,6 +52,9 @@ exports.getEditProduct = (req, res, next) => {
             if (!product) {
                 return res.redirect('/');
             }
+            if (product.userId.toString() !== req.user._id.toString()){
+                return res.redirect('/');
+            }
             res.render('admin/edit-product', {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
@@ -70,26 +73,25 @@ exports.postEditProduct = (req,res,next) => {
     
     Product.findById(prodId)
         .then(product => {
+            if (product.userId.toString() !== req.user._id.toString()){
+                return res.redirect('/');
+            }
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDescription;
             product.imageUrl = updatedImageUrl;
-            return product.save();
-        }).then(result => {
-            console.log('Product Updated');
-            res.redirect('/admin/products');
+            return product.save().then(result => {
+                console.log('Product Updated');
+                res.redirect('/admin/products');
+            });
         }).catch(err => console.log(err));   
 };
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
-        .then(result => {
-            console.log(result);
-        })
+    Product.deleteOne({_id: prodId, userId: req.user._id})
         .then(() => {
             console.log("Product Removed");
             res.redirect('/admin/products');
         }).catch(err => console.log(err));
-    
 }
